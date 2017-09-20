69603f1faf9cbc3e13ae35ac6ec5256fb4c11166
SoftwareEngineeringPractice2017 <31974523+SoftwareEngineeringPractice2017@users.noreply.github.com>
2017-09-21 00:15:58 +1000

    Add if condition to not show a marker if it does not have an icon image assigned

--- app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MapsActivity.java
+++ app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MapsActivity.java
@@ -323,6 +323,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 //The icon to display for this location.
                 int icon_icon = R.mipmap.ic_launcher; //R.mpmap.ic_launcher is temporary until icons are added.
 
+                // Boolean to say whether to add the marker or not
+                boolean bAddMarker = true;
+
                 //Set icon_icon to the right icon.
                 switch(i.getType())
                 {
@@ -336,14 +339,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         icon_icon = R.mipmap.telephone;
                         break;
                     default :
-                        icon_icon = R.mipmap.ic_launcher;
+                        bAddMarker = false;
                         break;
                 }
 
-                final Marker iconMarker = mMap.addMarker(new MarkerOptions()
-                        .position(i.getLocation())
-                        .icon(BitmapDescriptorFactory.fromResource(icon_icon))
-                );
+                if( bAddMarker )
+                {
+                    final Marker iconMarker = mMap.addMarker(new MarkerOptions()
+                            .position(i.getLocation())
+                            .icon(BitmapDescriptorFactory.fromResource(icon_icon))
+                    );
+                }
             }
         }
 
