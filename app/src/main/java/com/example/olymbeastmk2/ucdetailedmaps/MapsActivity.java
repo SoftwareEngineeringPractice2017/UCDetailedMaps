abc10e691e0ba24e2bb84a527ce8d45f444cff5b
SoftwareEngineeringPractice2017 <31974523+SoftwareEngineeringPractice2017@users.noreply.github.com>
2017-09-24 07:57:57 +1000

    Added Location Services ( Not working yet ). Also added Panda icon for lols

--- app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MapsActivity.java
+++ app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MapsActivity.java
@@ -1,12 +1,19 @@
 package com.example.olymbeastmk2.ucdetailedmaps;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.text.Editable;
@@ -19,9 +26,16 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.graphics.BitmapFactory;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,6 +46,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -47,6 +63,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //Location Stuff
     private FusedLocationProviderClient mFusedLocationClient;
 
+    private LocationRequest mLocationRequest;
+
+    // 10 Seconds
+    private long UPDATE_INTERVAL = 10 * 1000;
+
+    // 2 Seconds
+    private long FASTEST_INTERVAL = 2 * 1000;
+
     //Database Stuff
     // A Handle to the applications resources
     public Resources resources;
@@ -56,12 +80,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     // Drawer stuff
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
-    private ArrayList<MenuItem> menuItems;
-    private ArrayList<MenuItem> currentMenu;
+    private ArrayList< MenuItem > menuItems;
+    private ArrayList< MenuItem > currentMenu;
 
     //Local stuff
-    private ArrayList<Building> buildings;
-    private HashMap<String, ArrayList<Icon>> icons;
+    private ArrayList< Building > buildings;
+    private HashMap< String, ArrayList< Icon > > icons;
+
+    public void getLastLocation()
+    {
+        // Get the last known recent location
+        FusedLocationProviderClient locationClient = LocationServices.getFusedLocationProviderClient( this );
+
+        locationClient.getLastLocation()
+                .addOnSuccessListener( new OnSuccessListener< Location >()
+        {
+            @Override
+            public void onSuccess( Location location )
+            {
+                // GPS Location can be null if GPS is switched off
+                if( location != null )
+                {
+                    onLocationChanged( location );
+                }
+            }
+        } )
+                .addOnFailureListener( new OnFailureListener()
+                {
+                    @Override
+                    public void onFailure( @NonNull Exception e )
+                    {
+                        Log.e( "UCDetailedMaps", "Error trying to last GPS location" );
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    public void onLocationChanged( Location location )
+    {
+        // The new location has now been determined
+        String msg = "Updated Location: " + Double.toString( location.getLatitude() ) + Double.toString( location.getLongitude() );
+        Toast.makeText( this, msg, Toast.LENGTH_SHORT ).show();
+
+        // A LatLnt Object for use with maps is now created!
+        LatLng latLng = new LatLng( location.getLatitude(), location.getLongitude() );
+    }
+
+    protected void startLocationUpdates()
+    {
+        // Create location request to start recieving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority( LocationRequest.PRIORITY_HIGH_ACCURACY );
+        mLocationRequest.setInterval( UPDATE_INTERVAL );
+        mLocationRequest.setFastestInterval( FASTEST_INTERVAL );
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest( mLocationRequest );
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient( this );
+        settingsClient.checkLocationSettings( locationSettingsRequest );
+
+        LocationServices.getFusedLocationProviderClient( this ).requestLocationUpdates( mLocationRequest,
+                new LocationCallback()
+                {
+                    @Override
+                    public void onLocationResult( LocationResult locationResult )
+                    {
+                        try
+                        {
+                            onLocationChanged( locationResult.getLastLocation() );
+                        }
+                        catch( SecurityException e )
+                        {
+                            checkPermissions();
+                        }
+                    }
+                },
+        Looper.myLooper() );
+    }
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -76,174 +176,187 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = ( SupportMapFragment ) getSupportFragmentManager()
                 .findFragmentById( R.id.map );
 
+        // Location help
+        // https://github.com/codepath/android_guides/wiki/Retrieving-Location-with-LocationServices-API
 
         // Location stuff.
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient( this );
 
         // Setup the drawer.
         InitializeDrawer();
 
         mapFragment.getMapAsync( this );
-
     }
 
     private void InitializeDrawer()
     {
         //Drawer Layout
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout = ( DrawerLayout ) findViewById( R.id.drawer_layout );
+        mDrawerList = ( ListView ) findViewById( R.id.left_drawer );
 
 
-
-        FloatingActionButton mFAB = (FloatingActionButton) findViewById(R.id.floatingActionButton);
-        mFAB.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton mFAB = ( FloatingActionButton ) findViewById( R.id.floatingActionButton );
+        mFAB.setOnClickListener( new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                mDrawerLayout.openDrawer(Gravity.START);
+            public void onClick( View v )
+            {
+                mDrawerLayout.openDrawer( Gravity.START );
             }
-        });
+        } );
 
         //Clear the drawer when it is opened.
-        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+        mDrawerLayout.addDrawerListener( new DrawerLayout.DrawerListener()
+        {
             @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
+            public void onDrawerSlide( View drawerView, float slideOffset )
+            {
 
             }
 
             @Override
-            public void onDrawerOpened(View drawerView) {
+            public void onDrawerOpened( View drawerView )
+            {
 
 
             }
 
             @Override
-            public void onDrawerClosed(View drawerView) {
-                EditText SearchBox = (EditText) findViewById(R.id.editText);
-                SearchBox.setText("");
+            public void onDrawerClosed( View drawerView )
+            {
+                EditText SearchBox = ( EditText ) findViewById( R.id.editText );
+                SearchBox.setText( "" );
             }
 
             @Override
-            public void onDrawerStateChanged(int newState) {
+            public void onDrawerStateChanged( int newState )
+            {
                 refreshDrawerListView();
             }
-        });
+        } );
 
-        EditText SearchBox = (EditText) findViewById(R.id.editText);
-        SearchBox.addTextChangedListener(new TextWatcher() {
+        EditText SearchBox = ( EditText ) findViewById( R.id.editText );
+        SearchBox.addTextChangedListener( new TextWatcher()
+        {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void beforeTextChanged( CharSequence charSequence, int i, int i1, int i2 )
+            {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onTextChanged( CharSequence charSequence, int i, int i1, int i2 )
+            {
 
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
+            public void afterTextChanged( Editable editable )
+            {
                 refreshDrawerListView();
             }
-        });
+        } );
 
 
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mDrawerList.setOnItemClickListener( new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                MenuItemClicked(i);
+            public void onItemClick( AdapterView< ? > adapterView, View view, int i, long l )
+            {
+                MenuItemClicked( i );
             }
-        });
+        } );
     }
 
     public void refreshDrawerListView()
     {
-        EditText SearchBox = (EditText) findViewById(R.id.editText);
+        EditText SearchBox = ( EditText ) findViewById( R.id.editText );
 
         String search = SearchBox.getText().toString();
 
         //This should be moved at a later point. Should only be called once.
         menuItems = GetMenuItems();
 
-        currentMenu = FilterMenuItems(menuItems, search);
+        currentMenu = FilterMenuItems( menuItems, search );
 
-        MenuAdapter menuAdapter = new MenuAdapter(this,
+        MenuAdapter menuAdapter = new MenuAdapter( this,
                 R.layout.drawer_list_item,
-                MenuItemsNames(currentMenu),
-                currentMenu);
+                MenuItemsNames( currentMenu ),
+                currentMenu );
 
-        mDrawerList.setAdapter(menuAdapter);
+        mDrawerList.setAdapter( menuAdapter );
     }
 
-    private ArrayList<MenuItem> GetMenuItems()
+    private ArrayList< MenuItem > GetMenuItems()
     {
         //ArrayList to be loaded and returned.
-        ArrayList<MenuItem> output = new ArrayList<MenuItem>();
+        ArrayList< MenuItem > output = new ArrayList< MenuItem >();
 
         //Add each building name as a MenuItem.
-        for(Building b : buildings)
+        for( Building b : buildings )
         {
-            output.add(new MenuItem(b.getID(), b.getName()));
+            output.add( new MenuItem( b.getID(), b.getName() ) );
         }
 
         //Each key in icons represents an IconType.
         //Cycle through each IconType and add each as a new MenuItem.
-        for(String s : icons.keySet())
+        for( String s : icons.keySet() )
         {
-            output.add(new MenuItem(s));
+            output.add( new MenuItem( s ) );
         }
 
         //Sort output alphabetically.
-        Collections.sort(output);
+        Collections.sort( output );
 
         return output;
     }
 
-    private ArrayList<MenuItem> FilterMenuItems(ArrayList<MenuItem> input, String filter)
+    private ArrayList< MenuItem > FilterMenuItems( ArrayList< MenuItem > input, String filter )
     {
         filter = filter.toLowerCase();
         //ArrayList to be loaded with MenuItems that start with filter.
-        ArrayList<MenuItem> output = new ArrayList<MenuItem>();
+        ArrayList< MenuItem > output = new ArrayList< MenuItem >();
 
         //Cycle through each MenuItem in input.
-        for(MenuItem m : input)
+        for( MenuItem m : input )
         {
-            if(m.text.toLowerCase().contains(filter))
+            if( m.text.toLowerCase().contains( filter ) )
             {
-                output.add(m);
+                output.add( m );
             }
         }
 
         return output;
     }
 
-    private ArrayList<String> MenuItemsNames(ArrayList<MenuItem> input)
+    private ArrayList< String > MenuItemsNames( ArrayList< MenuItem > input )
     {
-        ArrayList<String> objects = new ArrayList<String>();
-        for(MenuItem m : input)
+        ArrayList< String > objects = new ArrayList< String >();
+        for( MenuItem m : input )
         {
-            objects.add(m.text);
+            objects.add( m.text );
         }
         return objects;
     }
 
-    private void MenuItemClicked(int position)
+    private void MenuItemClicked( int position )
     {
         float zoomLevel = 9;
-        LatLng location = new LatLng(0,0);;
+        LatLng location = new LatLng( 0, 0 );
+        ;
 
-        MenuItem menuItem = currentMenu.get(position);
+        MenuItem menuItem = currentMenu.get( position );
 
-        if(menuItem.type == MenuItem.ItemType.Building)
+        if( menuItem.type == MenuItem.ItemType.Building )
         {
-            for (Building b : buildings)
+            for( Building b : buildings )
             {
-                if(b.getID() == menuItem.id)
+                if( b.getID() == menuItem.id )
                 {
-                    location = LatLngTools.getCenter(b.getOutline());
+                    location = LatLngTools.getCenter( b.getOutline() );
                 }
             }
-        }
-        else
+        } else
         {
 
         }
@@ -251,6 +364,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera( CameraUpdateFactory.newLatLngZoom( location, zoomLevel ) );
     }
 
+
+    private boolean checkPermissions()
+    {
+        if( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED )
+        {
+            return true;
+        }
+        else
+        {
+            requestPermissions();
+            return false;
+        }
+    }
+
+    private void requestPermissions()
+    {
+        // KEEP AN EYE ON THIS LINE, INT SHOULD PROBABLY NOT BE 0
+        ActivityCompat.requestPermissions( this, new String[]{ android.Manifest.permission.ACCESS_FINE_LOCATION }, 0 );
+    }
 
     /**
      * Manipulates the map once available.
@@ -330,7 +462,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Scaled Water bitmap
         Bitmap waterBitmapOriginal = BitmapFactory.decodeResource(getResources(), R.mipmap.water);
-        double scaleFactor = 0.25;
+        double scaleFactor = 0.5;
         int newWidth = ( int ) ( scaleFactor * waterBitmapOriginal.getWidth() );
         int newHeight = ( int ) ( scaleFactor * waterBitmapOriginal.getHeight() );
         Bitmap waterBitmap = Bitmap.createScaledBitmap( waterBitmapOriginal, newWidth, newHeight, true );
