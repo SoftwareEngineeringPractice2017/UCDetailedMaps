e736e6cc7887fabf4771c49374f9dc01744b2170
Riley <u3100232@uni.canberra.edu.au>
2017-10-05 15:42:41 +1100

    [Start Work] Begin work on MenuHandler class.

---
+++ app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MenuHandler.java
@@ -0,0 +1,23 @@
+package com.example.olymbeastmk2.ucdetailedmaps;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Riley on 05/10/2017.
+ */
+
+public class MenuHandler {
+
+    private ArrayList< MenuItem > menuItems;
+    private ArrayList< Boolean > checkedState;
+
+    private ArrayList< MenuItem > currentMenu;
+
+
+    
+
+
+
+
+
+}
