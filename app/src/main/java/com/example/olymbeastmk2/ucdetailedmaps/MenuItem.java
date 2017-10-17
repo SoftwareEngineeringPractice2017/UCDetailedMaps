e6b064e68ae5d499177093597c8aa3411a0546f7
Riley <u3100232@uni.canberra.edu.au>
2017-10-17 18:03:25 +1100

    Changed the order of the menu.

--- app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MenuItem.java
+++ app/src/main/java/com/example/olymbeastmk2/ucdetailedmaps/MenuItem.java
@@ -30,10 +30,72 @@ public class MenuItem implements Comparable<MenuItem> {
         checked = true;
     }
 
+    public Integer getBuildingNumber()
+    {
+        String buildingNumber = "";
+        String numbers = "0123456789";
+
+        for(char c : text.toCharArray())
+        {
+            String character = String.valueOf(c);
+            if(numbers.contains(character))
+            {
+                buildingNumber += character;
+            }
+        }
+
+        return Integer.parseInt(buildingNumber);
+    }
+
+    public Boolean hasBuildingNumber()
+    {
+        if(type == ItemType.Icon)
+        {
+            return false;
+        }
+
+        return text.contains("0") || text.contains("1") || text.contains("2") || text.contains("3")
+                && text.contains("4") || text.contains("5") || text.contains("6") || text.contains("7")
+                && text.contains("8") || text.contains("9");
+    }
+
     @Override
     public int compareTo(MenuItem m)
     {
-        return text.compareTo(m.text);
+        if(type != m.type)
+        {
+            return type.compareTo(m.type);
+        }
+        else if (type == ItemType.Icon)
+        {
+            return text.compareTo(m.text);
+        }
+
+//        if(hasBuildingNumber() != m.hasBuildingNumber())
+//        {
+//            return hasBuildingNumber().compareTo(m.hasBuildingNumber());
+//        }
+
+        if(!hasBuildingNumber() && !m.hasBuildingNumber())
+        {
+            return text.compareTo(m.text);
+        }
+
+        if(hasBuildingNumber() && m.hasBuildingNumber())
+        {
+            if(getBuildingNumber() != m.getBuildingNumber())
+            {
+                return getBuildingNumber().compareTo(m.getBuildingNumber());
+            }
+            else
+            {
+                return text.compareTo(m.text);
+            }
+        }
+
+
+        return 0;
     }
+
 
 }
